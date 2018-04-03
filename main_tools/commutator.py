5780f264ec0fe85bf7fd232a9e289c6de6d5c918
aasthan4 <aasthan4@jhu.edu>
2018-04-02 22:09:55 -0400

    test cases edited

--- main_tools/commutator.py
+++ main_tools/commutator.py
@@ -24,7 +24,7 @@ class list_op_term:
 #comm should accept a list of terms/Alphabet operator and list of terms/Alphabet operator.
 #1-> commutator on 2-> commutator off so only 1 term on-> whether commutator is on or it is just a product
 # last-> if its the outside last commutator or not (for taking the fully contracted terms)
-def comm(a,b,on,last):
+def omm(a,b,on,last):
     #????assume prefactor of the term to be 1
     fc=1.0
 
