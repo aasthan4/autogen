1d95aef9c2d9ad84e3d015585bb56e081549342b
aasthan4 <aasthan4@jhu.edu>
2018-04-08 17:21:21 -0400

    added commutator rest and non diagonal / and rank of an operator to library

--- debug.py
+++ debug.py
@@ -3,9 +3,17 @@ from library import convert_pqr
 import main_tools
 from library import pick
 from library import print_terms
-list_terms=math.prod(['V2'],['T2'],1)
+from library import rn_comm
+list_terms=math.prod(['V2'],['T1'],1)
 list_terms=convert_pqr.convert_pqr(list_terms)
-list_terms=pick.pick(list_terms, ['i'],['j'])
+#list_terms=pick.pick(list_terms, ['i'],['j'])
 print '------'
+print_terms.print_terms(list_terms)
+print '------'
+#for item in list_terms:
+#    print rn_comm.rank(item)
+
+list_terms=rn_comm.select_r(list_terms)
+
 print_terms.print_terms(list_terms)
 
