8d54075e5c3d31cf84042df4134e41561d099aaf
aasthan4 <aasthan4@jhu.edu>
2018-04-10 12:07:48 -0400

    nothing imp

--- input.py
+++ input.py
@@ -8,11 +8,11 @@ from library import pick
 from library import compare_envelope as ce
 import library
 #ccsd.amplitude()
-
-list_terms=math1.comm(math1.comm(['V2'],['T1'],0),['T1'],1)
-list_terms.extend(math1.comm(math1.comm(['V2'],['T1'],0),['D1'],-0.5))
+list_terms=[]
+#list_terms=math1.comm(math1.comm(['V2'],['T1'],0),['T1'],1)
+#list_terms.extend(math1.comm(math1.comm(['V2'],['T1'],0),['D1'],-0.5))
 list_terms.extend(math1.comm(math1.comm(['V2'],['D1'],0),['T1'],-0.5))
-list_terms.extend(math1.comm(math1.comm(['V2'],['D1'],0),['D1'],1))
+#list_terms.extend(math1.comm(math1.comm(['V2'],['D1'],0),['D1'],1))
 list_terms=library.convert_pqr.convert_pqr(list_terms)
 list_terms=pick.pick(list_terms,['i'],['j'])
 list_terms=ce.compare_envelope(list_terms,1,1)
