fd2090ace9674bb46d56111493ee68f283c3aa3e
aasthan4 <aasthan4@jhu.edu>
2018-04-16 20:25:28 -0400

    only test case

--- debug.py
+++ debug.py
@@ -1,13 +1,15 @@
 from main_tools import product as math
 from library import convert_pqr  
-import main_tools
+from  main_tools.commutator import comm
 from library import pick
 from library import print_terms
 from library import rn_comm
-list_terms=math.prod(['V2'],['T2'],1)
+list_terms=comm(['V2'],['T2'],1)
+'''
 list_terms=convert_pqr.convert_pqr(list_terms)
 list_terms=print_terms.clean_list(list_terms)
 #list_terms=pick.pick(list_terms, ['i'],['j'])
+'''
 print '------'
 print_terms.print_terms(list_terms)
 print '------'
